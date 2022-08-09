b22931117731a5b914dc25f3a3d376fb02382be0
tabythas <tabythasmith@simmons.edu>
2022-08-09 18:08:57 -0400

    Added comments, log file, modified aircraft array

--- Final_PLP.cpp
+++ Final_PLP.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
 using namespace std;
 
 /* Flight path calculator 
@@ -10,38 +12,60 @@ using namespace std;
 -Function to return total distance and time
 */
 
+// struct to hold the coordinates so that x and y can be stored together
 struct Coordinates
 {
     float x;
     float y;
 };
 
+// function to pass in the user's choice to a switch statement and return correct speed value
 int userChoice(int);
+// function to take in total distance and time, calc and display
 void getTotal(double, double);
 
 int main()
 {
+    // create log file
+    fstream planeLog;
+    /// open and write to log file
+    planeLog.open("planelog.txt", ios::out);
+
+    // variables
     int choice, arrIndex = 0, total = 0, speed;
+    const int SIZE = 4;
+    string airplanes[SIZE] = {"A-10", "Piper Cub", "Boeing 747", "Decathlon"};
     const int ARRAY_XY = 100;
     Coordinates waypoints[ARRAY_XY];
     char again;
-    double a2, b2, c2, powerX, powerY, totalDistance =0, totalTime = 0;
-
+    double a2, b2, c2, powerX, powerY, totalDistance = 0, totalTime = 0;
+    
     cout << "### Flight Path Calculator ###\n";
-    cout<<"To begin, choose an aircraft by entering the associated number...\n";
-    cout<<"1) A-10\n2) Piper Cub\n3) Boeing 747\n4) Decathlon\nAircraft choice: ";
-    cin>>choice;
-    while (choice <= 0 || choice > 5){
+    cout<<"To begin, choose an aircraft from the following list by entering the associated number...\n";
+
+    for (int count = 0; count < SIZE; count++)
+        cout<<"Number "<<(count+1)<<": "<<airplanes[count]<<endl;
+
+    // validate user input
+    while (choice <= 0 || choice > 4){
         cout<<"Invalid input. Enter a correct number: ";
         cin>>choice;
     }
     cout<<endl;
+
+    // write aircraft to log file
+    planeLog<<"Aircraft: "<<airplanes[choice-1]<<endl;
+
+    // pass choice to function
     speed = userChoice(choice);
     cout<<"That aircraft flies at "<<speed<<" m/s (meters per second).";
     cout<<endl;
+    // write speed to log file
+    planeLog<<"Speed flown: "<<speed<<endl;
     cout << "Enter coordinates when prompted. Press enter to exit entering values.\n";
     cout << endl;
- 
+
+    // do-while loop to collect coordinates as long as the user wishes (array caps at 100)
     do
     {
         cout<<"Waypoint #"<<(arrIndex + 1)<<" X: ";
@@ -49,16 +73,22 @@ int main()
         cout<<"Waypoint #"<<(arrIndex + 1)<<" Y: ";
         cin>>waypoints[arrIndex].y;
         cout<<"("<<waypoints[arrIndex].x<<", "<<waypoints[arrIndex].y<<")"<<endl;
+        // write coordinates to log file
+        planeLog<<"Coordinate "<<(arrIndex + 1)<<": ";
+        planeLog<<fixed;
+        planeLog<<"("<<waypoints[arrIndex].x<<", "<<waypoints[arrIndex].y<<")"<<endl;
         cout<<endl;
         arrIndex++;
         total++;
         cout<<"Do you want to enter another coordinate? (Y/N): ";
         cin>>again; 
-
+        
     } while (again == 'Y' || again == 'y');
-
+    
+    // iterate through each coordinate
     for (int index = 1; index < total; index++)
     {
+        // formula to gather waypoint data 
         a2 = (waypoints[index].x - waypoints[(index - 1)].x);
         powerX = (a2 * a2);
         b2 = (waypoints[index].y - waypoints[(index - 1)].y);
@@ -73,14 +103,17 @@ int main()
         totalTime += time;
         cout << endl;
     }
-
+    // pass data to function
     getTotal(totalDistance, totalTime);
 
+    // close log file and end program
+    planeLog.close();
     return 0;
 }
 
 int userChoice(int choice)
 {
+    // default set to 0
     int speed = 0;
 
     switch (choice)
